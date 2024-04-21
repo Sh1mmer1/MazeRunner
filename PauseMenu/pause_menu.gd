9089c93d47a36e9538830e3cd4c0cbb1bcf157e6
deibar5 <deimantas.barstys@ktu.edu>
2024-04-21 20:28:19 +0300

    Added pause menu
    main

---
+++ PauseMenu/pause_menu.gd
@@ -0,0 +1,23 @@
+class_name PauseMenu
+
+extends Control
+
+func _on_continue_button_down():
+	get_tree().paused = false
+	queue_free()
+
+func _ready():
+	get_tree().paused = true
+
+func _on_restart_button_down():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://World/main.tscn")
+
+func _on_quit_to_main_button_down():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://World/menu.tscn")
+
+func _on_quit_game_button_down():
+	get_tree().paused = false
+	get_tree().quit()
+	
