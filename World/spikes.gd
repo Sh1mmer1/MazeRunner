4bfae244c12fba7d0d9386a8e74667884da6327e
ga8a <gabija.orlaite@gmail.com>
2024-05-11 19:20:44 +0300

    Spikes

---
+++ World/spikes.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_body_entered(body):
+	if body.has_method("die"):
+		$AnimatedSprite2D.play("activate")
+		body.die()
+
