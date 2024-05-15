8fab46238e2851d6af6683aeaabca6294c1b4767
ga8a <gabija.orlaite@gmail.com>
2024-05-15 12:45:16 +0300

    AnimationTree

--- Charecters/Player/Player.gd
+++ Charecters/Player/Player.gd
@@ -10,6 +10,7 @@ var is_dead: bool = false
 var animated_sprite: AnimatedSprite2D
 var body_sprite: Sprite2D
 var is_animating: bool = false
+var is_walking: bool = false
 var enemy: Enemy = null
 var is_gun_picked_up:bool = false
 var gun_cooldown = true
@@ -33,6 +34,7 @@ func _ready():
 	animated_sprite.visible = false	
 	body_sprite.visible = false
 	animation_tree.set("parameters/Walk/blend_position", 0.0)
+	animation_tree.set("parameters/Walk_Gun/blend_position", 0.0)
 	if spaceship == null:
 		body_sprite.visible = true
 		animation_tree.get("parameters/playback").travel("Idle")
@@ -59,16 +61,26 @@ func _physics_process(delta):
 		$Marker2D.look_at(direction)
 
 		if direction != Vector2.ZERO:
+			play_footsteps()
+			is_walking = true
 			velocity = direction * speed * delta * 3
 			animation_tree.set("parameters/Walk/blend_position", direction)
 			animation_tree.get("parameters/playback").travel("Walk")
 			if is_gun_picked_up:
+				animation_tree.set("parameters/Walk_Gun/blend_position", direction)
 				animation_tree.get("parameters/playback").travel("Walk_Gun")
+			
 			#$AnimationTree.set("parameters/Walk/blend_position", velocity)
 			#update_sprite_direction(direction)
+		
+
 		else:
 			velocity = Vector2.ZERO
+			$footstep.stop()
+			is_walking = false
 			animation_tree.get("parameters/playback").travel("Idle")
+			if is_gun_picked_up:
+				animation_tree.get("parameters/playback").travel("Idle_Gun")
 			if is_animating:
 				animated_sprite.stop()
 				is_animating = false
@@ -80,12 +92,19 @@ func _physics_process(delta):
 					animation_tree.get("parameters/playback").travel("Idle")
 			
 		move_and_slide()
+	
 		
 
 	queue_redraw()
 	if target:
 		aim()
-
+		
+func play_footsteps():
+	if $Timer.time_left <= 0:
+		$footstep.pitch_scale = randf_range(0.8, 1.2)
+		$footstep.play()
+		$Timer.start(0.4)
+		
 func aim():
 	var space_state = get_world_2d().get_direct_space_state()
 	var params = PhysicsRayQueryParameters2D.new()
@@ -101,33 +120,33 @@ func aim():
 			if gun_cooldown:
 				fire_laser(target.position)
 
-func update_sprite_direction(direction):
-	var animation_name = "" 
+#func update_sprite_direction(direction):
+#	var animation_name = "" 
 
-	if velocity.y > 0:
-		animation_name = "front_walk"
-	elif velocity.y < 0:
-		animation_name = "back_walk"
-	elif velocity.x > 0:
-		animation_name = "right_walk"
-	elif velocity.x < 0:
-		animation_name = "left_walk"
+#	if velocity.y > 0:
+#		animation_name = "front_walk"
+#	elif velocity.y < 0:
+#		animation_name = "back_walk"
+#	elif velocity.x > 0:
+#		animation_name = "right_walk"
+#	elif velocity.x < 0:
+#		animation_name = "left_walk"
 		
-	if is_gun_picked_up:
-		animation_name += "_gun"
-	
-	
-	if not is_animating:
-		#animated_sprite.play(animation_name)
-		is_animating = true
-		body_sprite.visible = true
-		animation_player.play(animation_name)
-		animated_sprite.visible = false
-	else:
-		if $Timer.time_left <= 0:
-			$footstep.pitch_scale = randf_range(0.8, 1.2)
-			$footstep.play()
-			$Timer.start(1)
+#	if is_gun_picked_up:
+#		animation_name += "_gun"
+#	
+#	
+#	if not is_animating:
+#		#animated_sprite.play(animation_name)
+#		is_animating = true
+#		body_sprite.visible = true
+#		animation_player.play(animation_name)
+#		animated_sprite.visible = false
+#	else:
+#		if $Timer.time_left <= 0:
+#			$footstep.pitch_scale = randf_range(0.8, 1.2)
+#			$footstep.play()
+#			$Timer.start(1)
 
 func die():
 	if not is_dead:
@@ -137,7 +156,7 @@ func die():
 		body_sprite.visible = true
 		animation_tree.get("parameters/playback").travel("Death")
 		$LoserAudio.play()
-		await animation_player.animation_finished
+		await animation_tree.animation_finished
 		Events.gameOver.emit()
 
 
